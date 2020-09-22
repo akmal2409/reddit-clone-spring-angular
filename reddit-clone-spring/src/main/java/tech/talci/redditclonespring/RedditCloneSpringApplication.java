fb2fcd14b1822e5015495c42ee008e8caefaf396
akmal2409 <akmalalikhujaev@gmail.com>
2020-09-22 19:17:00 +0200

    Initial commit

---
+++ reddit-clone-spring/src/main/java/tech/talci/redditclonespring/RedditCloneSpringApplication.java
@@ -0,0 +1,13 @@
+package tech.talci.redditclonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneSpringApplication.class, args);
+    }
+
+}
---
+++ reddit-clone-spring/src/test/java/tech/talci/redditclonespring/RedditCloneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.redditclonespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
