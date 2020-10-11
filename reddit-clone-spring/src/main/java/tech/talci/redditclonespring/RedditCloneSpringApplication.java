7ea1f8fe1b775a1c7fa6e4f39e7d343d94cb5afa
akmal2409 <akmalalikhujaev@gmail.com>
2020-10-11 15:01:18 +0200

    Added swagger and documented all API endpoints as well as disabled security for them. Added CORS mapping so angular frontend can communicate with backend. Added signup component

--- reddit-clone-spring/src/main/java/tech/talci/redditclonespring/RedditCloneSpringApplication.java
+++ reddit-clone-spring/src/main/java/tech/talci/redditclonespring/RedditCloneSpringApplication.java
@@ -2,10 +2,13 @@ package tech.talci.redditclonespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import tech.talci.redditclonespring.config.SwaggerConfig;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfig.class)
 public class RedditCloneSpringApplication {
 
     public static void main(String[] args) {
